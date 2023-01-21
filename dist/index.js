c814497555f201ec6a54941027a71e880dcbae29
kermage <genealyson.torcende@gmail.com>
2023-01-21 09:59:28 +0800

    ensure manifest generation on build and remove url file on dev exit

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const vite_1 = require("vite");
 const path_1 = require("path");
 const fs_1 = require("fs");
 function themeplate() {
     return {
         name: 'vite-plugin-themeplate',
         enforce: 'post',
+        config(config) {
+            return (0, vite_1.mergeConfig)(config, {
+                build: {
+                    manifest: true,
+                },
+            });
+        },
         configureServer(server) {
             const { config, httpServer } = server;
+            const outDir = (0, path_1.resolve)(config.root, config.build.outDir);
+            const outFile = (0, path_1.resolve)(outDir, 'themeplate');
+            const clean = () => {
+                if ((0, fs_1.existsSync)(outFile)) {
+                    (0, fs_1.rmSync)(outFile);
+                }
+            };
+            process.on('exit', clean);
+            process.on('SIGINT', process.exit);
+            process.on('SIGTERM', process.exit);
+            process.on('SIGHUP', process.exit);
             httpServer?.once('listening', () => {
                 const checker = setInterval(() => {
                     if (null !== server.resolvedUrls) {
-                        const outDir = (0, path_1.resolve)(config.root, config.build.outDir);
-                        const outFile = (0, path_1.resolve)(outDir, 'themeplate');
                         if (!(0, fs_1.existsSync)(outDir)) {
                             (0, fs_1.mkdirSync)(outDir);
                         }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,41 @@
+import { mergeConfig } from 'vite';
 import { resolve } from 'path';
-import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import { existsSync, mkdirSync, rmSync, writeFileSync } from 'fs';
 
-import type { Plugin, ViteDevServer } from 'vite';
+import type { Plugin, UserConfig, ViteDevServer } from 'vite';
 
 export default function themeplate(): Plugin {
 	return {
 		name: 'vite-plugin-themeplate',
 		enforce: 'post',
 
+		config( config: UserConfig ) {
+			return mergeConfig( config, {
+				build: {
+					manifest: true,
+				},
+			} )
+		},
+
 		configureServer( server: ViteDevServer ) {
 			const { config, httpServer } = server;
+			const outDir = resolve( config.root, config.build.outDir );
+			const outFile = resolve( outDir, 'themeplate' );
+
+			const clean = () => {
+				if ( existsSync( outFile ) ) {
+					rmSync( outFile );
+				}
+			}
+
+			process.on( 'exit', clean );
+			process.on( 'SIGINT', process.exit );
+			process.on( 'SIGTERM', process.exit );
+			process.on( 'SIGHUP', process.exit );
 
 			httpServer?.once( 'listening', () => {
 				const checker = setInterval( () => {
 					if ( null !== server.resolvedUrls ) {
-						const outDir = resolve( config.root, config.build.outDir );
-						const outFile = resolve( outDir, 'themeplate' );
-
 						if ( ! existsSync( outDir ) ) {
 							mkdirSync( outDir );
 						}
