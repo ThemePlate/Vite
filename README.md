cf50bb93d81607e15abd378b3362a4dc144f6a84
kermage <genealyson.torcende@gmail.com>
2023-01-22 17:38:51 +0800

    handle assets enqueue with custom data type module

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ use ThemePlate\Vite;
 add_action( 'wp_enqueue_scripts', function() {
 	$vite = new Vite( get_stylesheet_directory() . '/dist/manifest.json', get_stylesheet_directory_uri() );
 
-	wp_enqueue_script( Vite::CLIENT );
-	wp_enqueue_style( 'main-style', $vite->path( 'src/main.css' ) );
-	wp_enqueue_script( 'main-script', $vite->path( 'src/main.js' ) );
+	$vite->style( 'main-style', 'src/main.css' );
+	$vite->script( 'main-script', 'src/main.js' );
+	$vite->action();
 } );
 ```
 
--- src/Vite.php
+++ src/Vite.php
@@ -9,11 +9,14 @@
 
 namespace ThemePlate;
 
+use ThemePlate\Enqueue\CustomData;
+
 class Vite {
 
 	protected string $basedir;
 	protected string $baseurl;
 	protected array $assets;
+	protected CustomData $customdata;
 
 	public const CLIENT = '@vite/client';
 
@@ -23,6 +26,8 @@ class Vite {
 		$this->basedir = basename( dirname( $manifest ) );
 		$this->baseurl = $baseurl;
 		$this->assets  = $this->parse( $manifest );
+
+		$this->customdata = new CustomData();
 
 		$this->init( $manifest );
 
@@ -75,6 +80,17 @@ class Vite {
 	}
 
 
+	public function action(): void {
+
+		if ( wp_script_is( self::CLIENT, 'registered' ) ) {
+			wp_enqueue_script( self::CLIENT );
+		}
+
+		$this->customdata->action();
+
+	}
+
+
 	public function asset( string $name ): array {
 
 		if ( ! isset( $this->assets[ $name ] ) ) {
@@ -88,7 +104,7 @@ class Vite {
 
 	public function path( string $name ): string {
 
-		if ( ! wp_script_is( self::CLIENT ) ) {
+		if ( ! wp_script_is( self::CLIENT, 'registered' ) ) {
 			$asset = $this->asset( $name );
 
 			if ( ! empty( $asset ) ) {
@@ -105,8 +121,30 @@ class Vite {
 
 		// phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
 		wp_register_script( self::CLIENT, trailingslashit( $localurl ) . self::CLIENT, array(), null, false );
+		$this->customdata->add( 'script', self::CLIENT, array( 'type' => 'module' ) );
 
 		$this->baseurl = $localurl;
+
+	}
+
+
+	public function style( string $handle, string $src, array $deps = array(), string $media = 'all' ): void {
+
+		// phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+		wp_enqueue_style( $handle, $this->path( $src ), $deps, null, $media );
+
+	}
+
+
+	public function script( string $handle, string $src, array $deps = array(), bool $in_footer = false ): void {
+
+		if ( wp_script_is( self::CLIENT, 'registered' ) ) {
+			$deps[] = self::CLIENT;
+		}
+
+		// phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+		wp_enqueue_script( $handle, $this->path( $src ), $deps, null, $in_footer );
+		$this->customdata->add( 'script', $handle, array( 'type' => 'module' ) );
 
 	}
 
